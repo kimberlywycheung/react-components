567af59bf8c9e03f09eb9b7e0f0b5823ac11fc50
Kimberly Cheung <kimberlywycheung@gmail.com>
2022-11-07 12:05:25 -0800

    React Components exercise done

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// TODO
+const { useState } = React;
+
+const App = () => (
+  <div>
+    <h3>My Grocery List</h3>
+    <GroceryList items = {['Cucumbers', 'Kale']} />
+  </div>
+);
+
+const GroceryList = (props) => {
+  return (
+      <ul>
+        {props.items.map((item) => (
+          <GroceryListItem item={item} />
+        ))}
+      </ul>
+  )
+};
+
+const GroceryListItem = (props) => {
+  const [isHoveredOver, setIsHover] = useState(false);
+
+  const style = {
+    fontWeight: isHoveredOver? 'bold' : 'normal'
+  };
+
+  return (
+    <li
+      style={style}
+      onMouseEnter={() => (setIsHover(true))}
+      onMouseLeave={() => (setIsHover(false))}
+    >
+      {props.item}
+    </li>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+// const Cucumbers = () => {
+//   return <li>Cucumbers</li>
+// };
+
+// const Kale = () => {
+//   return <li>Kale</li>
+// };
